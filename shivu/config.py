64a6db3bcddc160295001fd98430b1ffc6671777
ManjirouSano69 <168046394+ManjirouSano69@users.noreply.github.com>
2024-05-23 22:02:37 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "1993048420", "5743248220", "1214348787", "5846676239", "5150045729", "1878911444", "5515750660", "5982968099"
     GROUP_ID = -1002023287369
     TOKEN = "6970757140:AAHsnyzgfk0ISxJWNnOdjO9WQxJX3Xj3tv8"
-    mongo_url = "mongodb+srv://nksharmas9835:Waifugrabbing@cluster0.amgwgh2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    mongo_url = "mongodb+srv://nksharmas9835:waifu@cluster0.ouufhhw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     PHOTO_URL = ["https://telegra.ph/file/19d22d4d58ebbd874d049.jpg", "https://telegra.ph/file/17fbb38ff98ed66e5826d.jpg"]
     SUPPORT_CHAT = "waifu_support_group"
     UPDATE_CHAT = "waifu_support_group"
